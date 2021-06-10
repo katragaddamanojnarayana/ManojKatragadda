55ec84207ea417ca53daa1f8f9749c21a37b6791
katragaddamanojnarayana <manoj4katragadda@gmail.com>
2021-06-10 14:11:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-d9vbyf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-d9vbyf)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Employee login</h1>
+      <lable>Employee Name :    </lable>
+      <input type="text" name="name"/><br/>
+
+      <lable>Password : </lable>
+      <input type="password" name="password"/><br/>
+
+      
+    </div>
+  );
+}
